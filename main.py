9fabdfc7a9bfbe9fea58ab5260fbd4526d9655f3
karthik <jakustove@gmail.com>
2025-07-17 17:06:39 +0530

    stage 3 completed

---
+++ main.py
@@ -0,0 +1,30 @@
+from turtle import Screen,Turtle
+from paddle import Paddle
+
+screen = Screen()
+screen.title("Pong")
+screen.bgcolor("Black")
+screen.setup(width=800,height=600)
+screen.tracer(0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
+
+
+
+
+screen.exitonclick()
